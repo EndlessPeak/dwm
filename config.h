a1b5960ac3954897eb4d5de50f64347626fb799a
LeeSin <endlesspeak@163.com>
2022-08-14 17:26:47 +0800

    patch gaps

--- config.h
+++ config.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -67,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -80,6 +85,19 @@ static Key keys[] = {
 	{ MODKEY,                       XK_k,      incnmaster,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY|ControlMask,           XK_l,      setmfact,       {.f = +0.05} },
+        { MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+        { MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+        { MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+        { MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
+        { MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+        { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+        { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+        { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+        { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+        { MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+        { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+        { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
